398067dc2f4ea3fe5a579283164bb759f4721361
VoteBem <32470677+VoteBem@users.noreply.github.com>
2017-11-06 21:16:08 -0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(".button-collapse").sideNav();
+
+//Select do cadastro
+$(document).ready(function() {
+    $('select').material_select();
+});
+//inicializacao do parallax
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+//inicialização do modal
+$(document).ready(function(){
+// the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+});
